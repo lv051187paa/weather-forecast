863be6d8eba8e82bc1bc95eba00b9dd299ff205d
Andrii Pavliuk <v051187paa@users.noreply.github.com>
2019-05-13 02:26:47 +0300

    Updates

---
+++ precache-manifest.ca1ae2d9adc3f563f985675c700034c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3c4ff10be43f35b53859257ac4c8b2",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "b4afe3d937721f4f10f8",
+    "url": "/weather-forecast/static/css/main.cca954cc.chunk.css"
+  },
+  {
+    "revision": "f6f38d5baf988b78b7c1",
+    "url": "/weather-forecast/static/js/2.56534743.chunk.js"
+  },
+  {
+    "revision": "b4afe3d937721f4f10f8",
+    "url": "/weather-forecast/static/js/main.c036e714.chunk.js"
+  },
+  {
+    "revision": "c753dce0acc1d73f011b",
+    "url": "/weather-forecast/static/js/runtime~main.ff5e48c9.js"
+  }
+]);
